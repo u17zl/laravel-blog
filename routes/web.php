aa60800ecd3f3117d0ee70e054542bfce374adb4
Kaiwen Luo <luokw8@gmail.com>
2019-06-10 23:30:05 +1000

    add admin controller

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//blog首页
 Route::get('/', function () {
     return redirect('/blog');
 });
 
 Route::get('/blog', 'BlogController@index')->name('blog.home');
 Route::get('/blog/{slug}', 'BlogController@showPost')->name('blog.detail');
+
+
+// 后台路由
+Route::get('/admin', function () {
+    return redirect('/admin/post');
+});
+
+Route::middleware('auth')->namespace('Admin')->group(function () {
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/tag', 'TagController');
+    Route::get('admin/upload', 'UploadController@index');
+});
+
+
+// 登录退出
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
