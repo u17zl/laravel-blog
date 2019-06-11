37a580903ab5903a8ffd4191fe9937751c67a9a4
Kaiwen Luo <luokw8@gmail.com>
2019-06-12 02:53:57 +1000

    tag CRUD

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
+// 在 TagController 顶部添加如下语句
+use App\Models\Tag;
+
+// 在TagController顶部添加这个use语句
+use App\Http\Requests\TagCreateRequest;
+
+// 在TagController顶部添加新的use语句
+use App\Http\Requests\TagUpdateRequest;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TagController extends Controller
 {
+    protected $fields = [
+        'tag' => '',
+        'title' => '',
+        'subtitle' => '',
+        'meta_description' => '',
+        'page_image' => '',
+        'layout' => 'blog.layouts.index',
+        'reverse_direction' => 0,
+    ];
+    
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +33,8 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $tags = Tag::all();
+        return view('admin.tag.index')->withTags($tags);
     }
 
     /**
@@ -24,18 +44,30 @@ class TagController extends Controller
      */
     public function create()
     {
-        //
+        $data = [];
+        foreach ($this->fields as $field => $default) {
+            $data[$field] = old($field, $default);
+        }
+
+        return view('admin.tag.create', $data);
     }
 
+    // 修改 store() 方法代码如下
     /**
-     * Store a newly created resource in storage.
+     * Store the newly created tag in the database.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param TagCreateRequest $request
+     * @return Response
      */
-    public function store(Request $request)
+    public function store(TagCreateRequest $request)
     {
-        //
+        $tag = new Tag();
+        foreach (array_keys($this->fields) as $field) {
+            $tag->$field = $request->get($field);
+        }
+        $tag->save();
+
+        return redirect('/admin/tag')->with('success', '标签「' . $tag->tag . '」创建成功.');
     }
 
     /**
@@ -50,36 +82,55 @@ class TagController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing a tag
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        $data = ['id' => $id];
+        foreach (array_keys($this->fields) as $field) {
+            $data[$field] = old($field, $tag->$field);
+        }
+
+        return view('admin.tag.edit', $data);
     }
 
+    // 替换 update() 方法如下
     /**
-     * Update the specified resource in storage.
+     * Update the tag in storage
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param TagUpdateRequest $request
+     * @param int $id
+     * @return Response
      */
-    public function update(Request $request, $id)
+    public function update(TagUpdateRequest $request, $id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+
+        foreach (array_keys(array_except($this->fields, ['tag'])) as $field) {
+            $tag->$field = $request->get($field);
+        }
+        $tag->save();
+
+        return redirect("/admin/tag/$id/edit")
+            ->with('success', '修改已保存.');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Delete the tag
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function destroy($id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        $tag->delete();
+
+        return redirect('/admin/tag')
+            ->with('success', '标签「' . $tag->tag . '」已经被删除.');
     }
 }
