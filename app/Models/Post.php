d029edc342da26397f99127e0223852d47496e3e
Kaiwen Luo <luokw8@gmail.com>
2019-06-19 01:11:32 +1000

    feat: markdown, doctrine, mixin pickadata.js and selectize.js

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,18 +2,86 @@
 
 namespace App\Models;
 
+use App\Services\Markdowner;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     protected $dates = ['published_at'];
 
+    /**
+     * The many-to-many relationship between posts and tags.
+     *
+     * @return BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag_pivot');
+    }
+
+    /**
+     * Set the title attribute and automatically the slug
+     *
+     * @param string $value
+     */
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
 
-        if (! $this->exists) {
-            $this->attributes['slug'] = str_slug($value);
+        if (!$this->exists) {
+            $value = uniqid(str_random(8));
+            $this->setUniqueSlug($value, 0);
         }
+    }
+
+    /**
+     * Recursive routine to set a unique slug
+     *
+     * @param string $title
+     * @param mixed $extra
+     */
+    protected function setUniqueSlug($title, $extra)
+    {
+        $slug = str_slug($title . '-' . $extra);
+
+        if (static::where('slug', $slug)->exists()) {
+            $this->setUniqueSlug($title, $extra + 1);
+            return;
+        }
+
+        $this->attributes['slug'] = $slug;
+    }
+
+    /**
+     * Set the HTML content automatically when the raw content is set
+     *
+     * @param string $value
+     */
+    public function setContentRawAttribute($value)
+    {
+        $markdown = new Markdowner();
+
+        $this->attributes['content_raw'] = $value;
+        $this->attributes['content_html'] = $markdown->toHTML($value);
+    }
+
+    /**
+     * Sync tag relation adding new tags as needed
+     *
+     * @param array $tags
+     */
+    public function syncTags(array $tags)
+    {
+        Tag::addNeededTags($tags);
+
+        if (count($tags)) {
+            $this->tags()->sync(
+                Tag::whereIn('tag', $tags)->get()->pluck('id')->all()
+            );
+            return;
+        }
+
+        $this->tags()->detach();
     }
 }
